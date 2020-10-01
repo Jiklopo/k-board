b35a45faed74b4a9ff3446a27e05f4c55ceb06df
Timur Khaimovich <itima.com@gmail.com>
2020-10-01 20:16:18 +0600

    Corrections

--- telegram/views.py
+++ telegram/views.py
@@ -1,9 +1,11 @@
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
 from rest_framework.views import Response
 from telegram.bot import bot
 
 
 @csrf_exempt
+@api_view(['POST'])
 def telegram_webhook(request):
     print(request.data)
     bot.process_new_updates(request.data)

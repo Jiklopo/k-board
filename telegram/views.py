4740d30879094dfeaf299b625cddfe3a97a5e787
Timur Khaimovich <itima.com@gmail.com>
2020-10-01 20:37:51 +0600

    Connected to Telegram Bot

--- telegram/views.py
+++ telegram/views.py
@@ -8,7 +8,6 @@ from telebot.types import Update
 @csrf_exempt
 @api_view(['POST'])
 def telegram_webhook(request):
-    print(request.data)
     update = Update.de_json(request.data)
     bot.process_new_updates([update])
     return Response(status=200)

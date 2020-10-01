6e8d4792ba90bd9f75767c7a09680ab3d1ef2d69
Timur <43034313+Jiklopo@users.noreply.github.com>
2020-10-01 19:55:20 +0600

    added csrf_exempt
    I hope this would work

--- telegram/views.py
+++ telegram/views.py
@@ -1,7 +1,9 @@
 from rest_framework.views import APIView, Response
 from telegram.bot import bot
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 class BotView(APIView):
     def post(self, request, format=None):
         print(request.data)
